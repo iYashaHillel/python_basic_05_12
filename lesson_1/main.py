3356083cc6e2324bc0983445826dd2d6f41fc0f2
iYasha <33287747+iYasha@users.noreply.github.com>
2022-12-05 21:25:15 +0200

    05.12

---
+++ lesson_1/main.py
@@ -0,0 +1,40 @@
+# One line comment
+10  # int (Integer)
+1.5  # float (Float)
+'Hello'  # str (String)
+True  # bool (Boolean)
+False  # bool (Boolean)
+
+# print('Hello')  # ctrl + /
+# print(1)
+# print(1.5)
+# print(True)
+# print('Hello', 'world', 2, 4.2, True, 4, 6, False)
+
+# a = 14
+# b = 20
+# c = 5 + 2
+#
+# print('B =', b)
+# print('C =', c)
+# print(a / b, a + 2)  # 14 / 20
+# print('A =', a)
+# A = 'Hello'
+# print(a, A)
+# PI_NUMBER = 3.14
+#
+# number_1 = 5
+# number_2 = 10
+# user_email = 'admin@admin.com'
+
+# print(10 + 2)
+# print(10 - 2)
+# print(10 * 2)
+# print(10 / 2)
+# print(5 // 2)
+# print(6 % 2)
+# print(6 ** 2)
+
+print('hello ' + 'world' + '2' + '2.5')
+print(('hello' + 'world ') * 3)
+
