484fb91324fb783764a07281ed0ed59d07c927a3
iYasha <33287747+iYasha@users.noreply.github.com>
2022-12-15 21:51:43 +0200

    15.12

---
+++ lesson_4/main.py
@@ -0,0 +1,101 @@
+
+# number_1 = int(input('Enter first number: '))
+# number_2 = int(input('Enter second number: '))
+
+# if number_1 > number_2:
+#     max_number = number_1
+# else:
+#     max_number = number_2
+
+# max_number = 'number_1' if number_1 > number_2 else 'number_2'
+
+# number_1 = 2
+# number_2 = 5
+# number_3 = 1
+
+# print('number_1' if number_1 > number_2 and number_1 > number_3 else number_2 if number_2 > number_1 and number_2 > number_3 else number_3)
+
+# print('left_value' if False else 'right_value')
+
+# if number_2 % 2 == 0:
+#     print('+')
+
+# print('+') if number_2 % 2 == 0 else ...
+
+# print('START')
+# number = 0
+# while number < 5:
+#     number += 1
+#     if number % 2 == 0:
+#         break
+#     print(number)
+# else:
+#     print('Else block')
+# print('END')
+
+# print('START')
+# name = ''
+# while name != 'q':
+#     name = input('Enter your name (q for Quit): ')
+#     print(f'Hello {name}!')
+# print('END')
+
+# print('START')
+# while True:
+#     name = input('Enter your name (q for Quit): ')
+#     if name == 'q':
+#         break
+#     print(f'Hello {name}!')
+# print('END')
+
+# number_sum = 0
+# for char in 'H123el223lo4':
+#     if char.isdigit():  # i in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):  # 'H' in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+#         print(char)
+#         number_sum += int(char)
+# print(number_sum)
+#
+# от, до, шаг (0 -> 10, 2 = (0, 2, 4, 6, 8))
+# до (0 -> 10, 1)
+# 10, 9, 8, 7, 6, 5, ..., 2, 1
+
+# 1, 2, 3, 4
+# string
+# range
+
+# number_sum = 0
+# for i in range(0, 1000, 2):
+    # if i % 2 == 0:  # 1000 -> 500
+#     number_sum += i
+# print(number_sum)
+
+# import random
+
+# print(random.uniform(0, 1))
+# print(random.randint(0, 10))
+#
+# a = 1
+# b = 10
+# random_number = int((2 ** 3 * 3.14 * a + 1) % b)
+# print(random_number)
+
+# import time
+#
+# print(time.time())  # timestamp 1 january 1970
+
+"""
+N = 5
+*****
+****
+***
+**
+*
+
+*
+**
+***
+****
+*****
+"""
+
+
